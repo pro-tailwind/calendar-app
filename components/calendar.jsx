28b4168e5d5511179c6e746202bcde88888bf4f6
Simon Vrachliotis <simon@simonswiss.com>
2022-08-11 15:17:05 +1000

    Removing Headless UI experiments

--- components/calendar.jsx
+++ components/calendar.jsx
@@ -43,12 +43,8 @@ export function Calendar(props) {
       <div className="flex items-center justify-between">
         <h2 className="text-lg font-semibold">{title}</h2>
         <div className="flex gap-2">
-          <CalendarButton {...prevButtonProps}>
-            <ChevronLeftIcon className="-ml-0.5 h-6 w-6" />
-          </CalendarButton>
-          <CalendarButton {...nextButtonProps}>
-            <ChevronRightIcon className="ml-0.5 h-6 w-6" />
-          </CalendarButton>
+          <CalendarButton {...prevButtonProps} />
+          <CalendarButton {...nextButtonProps} />
         </div>
       </div>
       <CalendarGrid state={state} />
@@ -182,11 +178,19 @@ function CalendarButton(props) {
   let ref = React.useRef()
   let { buttonProps } = useButton(props, ref)
 
+  const direction = buttonProps['aria-label']
+
   return (
     <button
       ref={ref}
       {...buttonProps}
       className="grid aspect-square w-12 max-w-full place-items-center rounded-full border border-slate-300 text-slate-400 hover:text-indigo-600 disabled:border-slate-200 disabled:text-slate-300 disabled:hover:text-slate-300"
-    />
+    >
+      {direction === 'Previous' ? (
+        <ChevronLeftIcon className="-ml-0.5 h-6 w-6" />
+      ) : (
+        <ChevronRightIcon className="ml-0.5 h-6 w-6" />
+      )}
+    </button>
   )
 }
