de429e4d307f92b1acdfc4d6ff70bf69b6eb1d3c
Simon Vrachliotis <simon@simonswiss.com>
2022-08-12 09:55:57 +1000

    Better looking theme switcher

--- components/theme-switcher.jsx
+++ components/theme-switcher.jsx
@@ -15,7 +15,7 @@ export function ThemeSwitcher({ activeTheme, setActiveTheme }) {
     <div className="fixed top-2 right-2 z-20 text-right">
       <Listbox value={activeTheme} onChange={setActiveTheme}>
         <div className="relative mt-1">
-          <Listbox.Button className="inline-flex w-full justify-center rounded-md bg-primary-700/30 px-4 py-2 text-sm font-medium text-white hover:bg-primary-700/50 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/50">
+          <Listbox.Button className="inline-flex w-full justify-center rounded-md bg-primary-700/30 px-4 py-2 hover:bg-primary-700/50 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/50">
             <ColorSwatchIcon className="h-5 w-5 text-primary-100" />
             <ChevronDownIcon
               className="ml-2 -mr-1 h-5 w-5 text-primary-100 hover:text-primary-50"
@@ -39,9 +39,9 @@ export function ThemeSwitcher({ activeTheme, setActiveTheme }) {
                   <span
                     data-theme={theme}
                     className={cx(
-                      'group flex w-full items-center py-2 pl-10 pr-4 text-sm font-medium capitalize',
-                      (active || selected) && 'bg-primary-200 text-primary-900',
-                      !active && !selected && 'text-slate-900'
+                      'group flex w-full items-center py-2 pl-10 pr-4 text-sm font-semibold capitalize',
+                      (active || selected) && 'bg-primary-100 text-primary-600',
+                      !active && !selected && 'text-slate-500'
                     )}
                   >
                     {theme}
