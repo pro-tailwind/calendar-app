7a8b49be8117a77d4ab872a19118396ecf867107
Simon Vrachliotis <simon@simonswiss.com>
2022-12-28 15:26:34 +1030

    Card container

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
 export default function Homepage() {
-  return <p>Let's build a calendar app!</p>
+  return (
+    <div className="grid min-h-screen place-items-center bg-cyan-500">
+      <div className="mx-auto w-full max-w-5xl px-6">
+        <div className="grid h-[540px] grid-cols-[340px,1fr] overflow-hidden rounded-2xl bg-white shadow-2xl">
+          <aside className="bg-gray-100"></aside>
+          <main className="bg-white"></main>
+        </div>
+      </div>
+    </div>
+  )
 }
