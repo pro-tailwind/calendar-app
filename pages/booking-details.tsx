ee2818b43c8ecbc4c97d424fb24bba4d1e01228a
Simon Vrachliotis <simon@simonswiss.com>
2022-12-28 14:17:46 +1030

    Booking details starting point

--- pages/booking-details.tsx
+++ pages/booking-details.tsx
@@ -2,9 +2,6 @@ import { useState } from 'react'
 import { useRouter } from 'next/router'
 import { useDateFormatter } from 'react-aria'
 
-import { Button } from '../components/button'
-import { Input, Textarea } from '../components/input'
-
 export default function BookingDetailsPage() {
   const router = useRouter()
   const [isLoading, setIsLoading] = useState(false)
@@ -25,39 +22,19 @@ export default function BookingDetailsPage() {
   function handleSubmit(event) {
     event.preventDefault()
     setIsLoading(true)
+    // Pretend to do some async tasks for 2.5 seconds :)
     setTimeout(() => {
-      router.push(`/confirmation?time=${time}`)
       setIsLoading(false)
     }, 2500)
   }
 
+  /*
+    TODOD:
+    Implement the Booking details screen
+  */
   return (
     <div className="p-10">
-      <h1 className="text-center text-2xl font-bold md:text-left">Let's confirm your booking!</h1>
-      <div className="mt-8 space-y-2">
-        <p>
-          You're about to book a one-hour meeting on{' '}
-          <strong className="text-cyan-600">{formattedTime}</strong>.
-        </p>
-        <p>Please fill in the form below to confirm.</p>
-      </div>
-
-      <div className="mt-20">
-        <form onSubmit={handleSubmit}>
-          <div className="grid grid-cols-2 gap-6">
-            <Input name="name" id="name" label="Name" required />
-            <Input name="email" id="email" label="Email" type="email" required />
-          </div>
-          <div className="mt-8">
-            <Textarea name="notes" label="Notes & Questions" id="notes" />
-          </div>
-          <div className="mt-8">
-            <Button type="submit" isLoading={isLoading} hasIcon>
-              Confirm booking
-            </Button>
-          </div>
-        </form>
-      </div>
+      <p>Booking details form</p>
     </div>
   )
 }
