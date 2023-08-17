f3dd28db4aff097fc1c134e5f71f740ab5fa42c7
Simon Vrachliotis <simon@simonswiss.com>
2023-08-17 10:10:03 +1000

    background subtle animation and ribbon focus ring + link to Pro Tailwind

--- src/components/ribbon.tsx
+++ src/components/ribbon.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link"
+
 export function Ribbon() {
   return (
     <div className="absolute -right-2 -top-2 z-10 aspect-square w-32 overflow-hidden rounded-sm">
@@ -7,9 +9,9 @@ export function Ribbon() {
         className="absolute bottom-0 right-0 h-2 w-2 bg-violet-500"
       ></div>
       <div className="absolute bottom-0 right-0 w-square-diagonal origin-bottom-right rotate-45">
-        <a
-          href="#"
-          className="flex flex-col items-center bg-violet-300 py-2.5 shadow hover:bg-violet-200 focus:outline-none focus:ring focus:ring-primary-400 focus:ring-offset-1"
+        <Link
+          href="https://protailwind.com"
+          className="flex flex-col items-center bg-violet-300 py-2.5 shadow hover:bg-violet-200 focus:outline-none focus:ring focus:ring-inset focus:ring-violet-500"
         >
           <span className="text-[10px] font-semibold uppercase leading-none tracking-wide text-violet-900/60">
             Powered by
@@ -17,7 +19,7 @@ export function Ribbon() {
           <span className="font-bold leading-none text-violet-900">
             Pro Tailwind
           </span>
-        </a>
+        </Link>
       </div>
     </div>
   )
